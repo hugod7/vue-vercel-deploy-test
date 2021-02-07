a969968ee764a2c09ce2e17811246accbc4592f3
Hugo David <hugod7@outlook.pt>
2021-02-07 11:58:51 +0000

    a

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Bem Vindo JotaSV Pato!"/>
   </div>
 </template>
 
